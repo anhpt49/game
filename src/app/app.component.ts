9d9fdbe3cb703a423febfdf44f681081e526de36
Phạm Tuấn Anh <anhpt.49@gmail.com>
2018-03-09 22:04:01 +0700

    Add code next level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  tiles = [];
 
-  tiles = [
-    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
-    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
-    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
-  ];
-
-  numOfColumn = 5;
+  numOfColumn = 2;
   num = this.numOfColumn * this.numOfColumn;
+
+  numOfTrue = 0;
+  numOfFalse = 0;
+
+  ngOnInit() {
+    this.tiles = this.randomBox(this.num);
+  }
+
   randomBox = (value) => {
     let a = [];
     const ramdomColor = this.getRandomColor();
     const randomNumber = this.getRandomInt(0, this.num);
     for (let i = 1; i <= value; ++i) {
       a.push(
-        { text: "", cols: 1, rows: 1, color: ramdomColor, opacity: i === randomNumber ? '0.8' : '1' }
+        { text: "", cols: 1, rows: 1, color: ramdomColor, opacity: i === randomNumber ? '0.9' : '1' }
       )
     }
     return a;
+  }
+
+  checkBox = (box: any) => {
+    if (box.opacity === '0.9') {
+      this.numOfTrue++;
+      if (this.numOfTrue % 5 === 0) {
+        this.numOfColumn++;
+      }
+      this.num = this.numOfColumn * this.numOfColumn;
+    }
+    else {
+      this.numOfFalse++;
+    }
+    this.tiles = this.randomBox(this.num);
   }
 
   getRandomColor = (): string => {
@@ -41,5 +57,7 @@ export class AppComponent {
   getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+
 
 }
